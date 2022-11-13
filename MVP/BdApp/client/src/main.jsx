5eaf3443b35c8abf4860262c52e16cb1d956bf8d
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-11-12 22:39:09 -0800

    Commented out Dependencies

--- MVP/BdApp/client/src/main.jsx
+++ MVP/BdApp/client/src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { TransactionProvider} from './context/TransactIonContext';
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+// import { TransactionProvider} from './context/TransactIonContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
