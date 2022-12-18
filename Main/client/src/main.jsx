4fcba6dff0e1df0fb0b021e7328c56bbf103f258
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-12-17 21:05:35 -0800

    Preparing to import other JSXs

--- Main/client/src/main.jsx
+++ Main/client/src/main.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import Home from './Home'
+// import Roadmap from './Roadmap'
+// import StayUpdated from './StayUpdated'
+
+// import HomeTest from './test/HomeTest'
+// import RoadmapTest from './test/RoadmapTest'
+// import StayUpdatedTest from './test/RoadmapTest'
+
 import './assets/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
+    {/* <Roadmap />
+    <StayUpdated />
+
+ Testing Jsx Below 
+    <HomeTest />
+    <RoadmapTest />
+    <StayUpdatedTest />     */}
   </React.StrictMode>,
 )
 
