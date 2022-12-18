19535636a1bd1f4a0566815dac22c7a450292334
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-12-17 20:24:34 -0800

    Trial and Error

--- Main/client/src/main.jsx
+++ Main/client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home'
 import './assets/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
