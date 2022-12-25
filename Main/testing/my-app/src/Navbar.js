8b88588cebb8caf47ca3fa104be854924e43c993
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-12-25 14:39:04 -0600

    Until Tomorrow

---
+++ Main/testing/my-app/src/Navbar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <div>
+                    <Link to='/'>
+                        
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
