c8ddc04f171c3aa7143339894722a1156b9300f1
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-12-26 21:02:43 -0600

    Why Isn't BrowserRouter rendering?

--- Main/testing/client/src/App.js
+++ Main/testing/client/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/pages/Home';
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import Home from './components/pages/Home';
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route exact path="/" component={Home}/>
-    //   </Routes>
-    // </Router>
-     <div className="App">
-       <h1>
-         Testing , 1,2,3
-       </h1>
-     </div>
+      // <Router>
+      //   <Routes>
+      //     <Route exact path="/" component={Home}/>
+      //   </Routes>
+      // </Router>
+       <div className="App">
+         <h1>
+           Testing , 1,2,3
+         </h1>
+       </div>
   );
 }
 
--- Main/testing/client/src/components/pages/Home.js
+++ Main/testing/client/src/components/pages/Home.js
@@ -1,76 +1,14 @@
 import React from 'react';
 // import '../assets/css/HomePage.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Home() {
 
   return (
     
-    <div id='home-container'> 
-      
-      <div id="above">
-      
-        <img class="icon" src="/icons&images/Icon.png"/>
-      
-        <h1 id="title">ecare</h1>
-        
-        <div id="name">
-          <p id="names">ethereum</p> 
-          <p id="names">combinatorial</p> 
-          <p id="names">auction</p> 
-          <p id="names">rate</p> 
-          <p id="names">exchange</p>
-        </div>
-        
-           {/* Change below link after test  */}
-        <a href="/prototype" id="prototype">
-          Prototype
-        </a>
-        
-        <br></br>
-        
-           {/* Change below link after test  */}
-        <a href="/guide" id="guide">
-          Guide
-        </a>
-      
-      </div>
-      
-      {/* <div id="about"> 
-        
-          <p id="about-desc">Created to house an ecosystem 
-          to lift storytellers out of poverty.</p>
-          
-      </div>	  */}
-        
-       <div id="ecosystem">
-      
-         {/* Change below link after test    */}
-        <a href="https://arells.com"
-          id="eco-link">		      
-          <img id="letter-logo" src="/icons&images/Arells-Logo.png"></img>	      
-        </a>		 
-    
-    
-      </div>
-      
-            
-      <div id="infrastructure">
-
-         {/* Change below link after test  */}
-        <Link to="/roadmap" id="roadmap">Roadmap
-        </Link>
-        
-        <br></br>
-              
-        <a href="https://github.com/orgs/Ecare-Exchange"
-          target="_blank"
-          id="contact">
-          <img id="github" src="/icons&images/GithubBright.png"></img>
-        </a>  	
-          
-      </div>
-    </div>
+    <>
+      <h1>Testing, 123</h1>
+    </>
     
   )
   
