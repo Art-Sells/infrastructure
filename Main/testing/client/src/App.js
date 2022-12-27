cf55578218a6acca07b64e6bd3e4d143c9b1b260
Jeremy Akatsa <jeyakatsa@gmail.com>
2022-12-27 01:10:00 -0600

    Tough times build strong people

--- Main/testing/client/src/App.js
+++ Main/testing/client/src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import './App.css';
- import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
- import Home from './components/pages/Home';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
-function App() {
+const Test = () => {
   return (
-      <>
-        {/* <Router>
-         <Routes>
-           <Route exact path='/' component={Home}/>
-         </Routes>
-       </Router> */}
-        <div id="App">
-          <h1>
-            Testing , 1,2,3
-          </h1>
-        </div>     
-      </>
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <div>
+          <Link to="/">Test</Link>
+        </div>
+
+
+
+        <Routes>
+          <Route path='/'exact> 
+            <Test/>          
+          </Route>
+        </Routes>
+      </Router>
+      {/* <div id="App">
+        <h1>
+          Testing , 1,2,3
+        </h1>
+      </div>      */}
+    </>
   );
 }
+
 
 export default App;
