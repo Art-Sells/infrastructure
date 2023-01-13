569b855f4394cfda53c2cc5b0769bc4c93c79606
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-01-12 23:29:11 -0800

    Moving onto new file to resolve Router Dom Issue.....

--- Main/test/src/App.js
+++ Main/test/src/App.js
@@ -1,10 +1,14 @@
-
+import React from "react";
+import Home from "./Home";
+import About from "./About";
 import './App.css';
 
 function App() {
   return (
     // Recovering from Covid.....
     <div className="App">
+      <Home/>
+      <About/>s
 
     </div>
   );
