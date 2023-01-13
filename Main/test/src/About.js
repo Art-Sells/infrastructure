03271f16a720a197da397dec2d7ba92423638d15
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-01-12 23:23:13 -0800

    Building New Test-layout

---
+++ Main/test/src/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+    return (
+        <H1>About</H1>
+    );
+}
+
+export default About;
---
+++ Main/test/src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+    return (
+        <H1>Home</H1>
+    );
+}
+
+export default Home;
