a8495cffe2e9ac4bbebe49de43e18f5c4e035cd6
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-01-23 23:00:52 -0800

    Building new Modal

--- Main/client/src/components/modals/Modal.jsx
+++ Main/client/src/components/modals/Modal.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-const Modal = () => {
+import React, {useState} from 'react';
+const Modal = ({isOpen, toggle}) => {
   return (
     <div id="Modal-Container">
+        {isOpen && (
         <div id="Modal-Content">
             <p id="Modal-Words"></p>
             <p id="Close" onClick={handleClose}>Ok</p>
         </div>
+        )}
     </div>
     
   );
 }
+
+export default Modal;
