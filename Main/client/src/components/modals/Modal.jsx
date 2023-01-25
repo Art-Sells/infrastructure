efcf9f397d6b18d2a1a7ec0a1eef45c0d31d506b
Jeremy Akatsa <jeyakatsa@gmail.com>
2023-01-24 18:11:07 -0800

    Added Modal CSS

--- Main/client/src/components/modals/Modal.jsx
+++ Main/client/src/components/modals/Modal.jsx
@@ -1,11 +1,13 @@
+import '../../css/components/Modal.css';
+
 import React, {useState} from 'react';
 const Modal = ({isOpen, toggle}) => {
   return (
     <div id="Modal-Container">
         {isOpen && (
         <div id="Modal-Content">
-            <p id="Modal-Words"></p>
-            <p id="Close" onClick={handleClose}>Ok</p>
+            <p id="Modal-Words"> Testing</p>
+            <p id="Modal-Close" onClick={handleClose}>Ok</p>
         </div>
         )}
     </div>
